34ae5b319bde9939c87380bd86f9be2620f0112e
jonson22 <jonatonstiff@gmail.com>
2021-08-25 15:53:27 +0900

    App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { Wrapper } from './style'
+import Navbar from './navbar.jsx'
+
+export default class App extends Component {
+  render() {
+    return (
+     <Wrapper>
+
+      <Navbar/>
+
+
+
+
+
+     </Wrapper>
+    )
+  }
+}
+
